f761dce169031a320b2b2cd73416ee1b20218ed9
Indunil1992 <indunil@adroitlogic.com>
2021-03-26 13:47:20 +0530

    6

--- induuu-m26.js
+++ induuu-m26.js
@@ -1,3 +1,6 @@
+const google = require('googleapis').google;
+const _auth = require('./Authorizer');
+const pubsub = google.pubsub('v1');
 const AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 
@@ -15,6 +18,25 @@ exports.handler = async (event) => {
     } catch (err) {
         // error handling goes here
     };
+    pubsub.projects.topics.subscriptions.list({
+        topic: `projects/${process.env.GCP_PROJECT}/topics/cloud-builds`,
+        pageSize: 10
+    })
+        .then(response => {
+            console.log(response.data);  // successful response
+            /*
+            response.data = {
+                "subscriptions": [
+                    "projects/<project>/subscriptions/<subscription-1>",
+                    "projects/<project>/subscriptions/<subscription-2>",
+                    ...
+                ]
+            }
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
 
-    return { "message": "Successfully executed" };
+    return { "message": "Successfully executed 11" };
 };
